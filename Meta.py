2144f4c7d127d29d1f78eec6efd30b9d5faead79
KiNGEX1 <144549465+KiNGEX1@users.noreply.github.com>
2023-09-10 18:08:30 +0530

    Update Meta.py

--- Meta.py
+++ Meta.py
@@ -65,7 +65,6 @@ pkg install ruby -y
 
 center() {
   termwidth=$(stty size | cut -d" " -f2)
-  padding="$(printf '%0.1s' ={1..500})"
   printf '%*.*s %s %*.*s\n' 0 "$(((termwidth-2-${#1})/2))" "$padding" "$1" 0 "$(((termwidth-1-${#1})/2))" "$padding"
 }
 
