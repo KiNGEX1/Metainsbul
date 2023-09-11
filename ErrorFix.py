3ddfa1097327473080dd6d462d7e9fe97dbbb2bf
KiNGEX1 <144549465+KiNGEX1@users.noreply.github.com>
2023-09-11 13:20:05 +0530

    Update ErrorFix.py

--- ErrorFix.py
+++ ErrorFix.py
@@ -14,3 +14,16 @@ os.system("pkg up")
 print("Fixed Metaspolit")
 print("__________________")
 print("Fixing Ngrok")
+print("1. Arch64")
+print("2. Arch32")
+architecture_choice = input("Enter your choice: ").strip()
+
+if architecture_choice == '1':
+    os.system("pkg install wget -y")
+  print("Ngrok Fixed")
+print("Turn On your Hotspot Before using the Ngrok")
+elif architecture_choice == '2':
+    os.system("pkg install wget -y")
+print("Ngrok Fixed")
+print("Turn On your Hotspot Before using the Ngrok")
+  
