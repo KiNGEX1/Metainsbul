6c7fd445e607ac1c58e86522bc577bce13cee603
KiNGEX1 <144549465+KiNGEX1@users.noreply.github.com>
2023-09-11 11:38:30 +0530

    Update NgrokStart.py

--- NgrokStart.py
+++ NgrokStart.py
@@ -7,9 +7,11 @@ ngrok_choice = input("Enter your choice: ").strip()
 
 if ngrok_choice == '1':
     port = input("Enter your port: ").strip()
+    os.system("cd $HOME")
     os.system(f"./ngrok http {port}")
 elif ngrok_choice == '2':
     port = input("Enter your port: ").strip()
+    os.system("cd $HOME")
     os.system(f"./ngrok tcp {port}")
 else:
     print("Invalid choice. Please select 1 or 2.")
