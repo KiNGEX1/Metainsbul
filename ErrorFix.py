9bac13309f5f914f8eb0127069324939239c5639
KiNGEX1 <144549465+KiNGEX1@users.noreply.github.com>
2023-09-11 13:14:37 +0530

    Create ErrorFix.py

---
+++ ErrorFix.py
@@ -0,0 +1,16 @@
+import os
+
+print("Fixing Ip tracker")
+os.system("apt update && apt upgrade -y")
+os.system("pip install requests")
+print("Fixed")
+print("__________________")
+print("Fixing Metaspolit")
+os.system("termux-setup-storage")
+os.system("pkg install ruby -y")
+os.system("pkg install wget curl openssh git -y")
+os.system("apt install ncurses-utils")
+os.system("pkg up")
+print("Fixed Metaspolit")
+print("__________________")
+print("Fixing Ngrok")
