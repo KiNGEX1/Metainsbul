84ddb2449fcf1d58ed0ab89e04cf0dfa829a43e8
KiNGEX1 <144549465+KiNGEX1@users.noreply.github.com>
2023-09-11 12:34:30 +0530

    Create iptrack.py

---
+++ iptrack.py
@@ -0,0 +1,27 @@
+import requests
+
+def ip_locator(ip_address):
+    url = f"http://ip-api.com/json/{ip_address}"
+    response = requests.get(url)
+    data = response.json()
+
+    if data["status"] == "success":
+        print(banner)
+        print("IP Address:", data["query"])
+        print("Country:", data["country"])
+        print("Region:", data["regionName"])
+        print("City:", data["city"])
+        print("Latitude:", data["lat"])
+        print("Longitude:", data["lon"])
+        print("ISP:", data["isp"])
+        
+        # Create a Google Maps link
+        google_maps_url = f"https://www.google.com/maps?q={data['lat']},{data['lon']}"
+        print("Google Maps URL:", google_maps_url)
+    else:
+        print("Failed to locate the IP address.")
+
+if __name__ == "__main__":
+    ip_address = input("Enter an IP address: ")
+    ip_locator(ip_address)
+    
