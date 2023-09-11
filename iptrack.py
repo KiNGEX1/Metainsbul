65bbaa6b71f165991cc08ec741a81251611b12ad
KiNGEX1 <144549465+KiNGEX1@users.noreply.github.com>
2023-09-11 12:51:51 +0530

    Update iptrack.py

--- iptrack.py
+++ iptrack.py
@@ -6,21 +6,29 @@ def ip_locator(ip_address):
     data = response.json()
 
     if data["status"] == "success":
-        print("IP Address:", data["query"])
-        print("Country:", data["country"])
-        print("Region:", data["regionName"])
-        print("City:", data["city"])
-        print("Latitude:", data["lat"])
-        print("Longitude:", data["lon"])
-        print("ISP:", data["isp"])
+        print("██╗██████╗░  ████████╗██████╗░░█████╗░░█████╗░██╗░░██╗")
+        print("██║██╔══██╗  ╚══██╔══╝██╔══██╗██╔══██╗██╔══██╗██║░██╔╝")
+        print("██║██████╔╝  ░░░██║░░░██████╔╝███████║██║░░╚═╝█████═╝░")
+        print("██║██╔═══╝░  ░░░██║░░░██╔══██╗██╔══██║██║░░██╗██╔═██╗░")
+        print("██║██║░░░░░  ░░░██║░░░██║░░██║██║░░██║╚█████╔╝██║░╚██╗")
+        print("╚═╝╚═╝░░░░░  ░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝")
+        print("\n[Location Information]\n")
+        print(f"IP Address: {data['query']}")
+        print(f"Country: {data['country']}")
+        print(f"Region: {data['regionName']}")
+        print(f"City: {data['city']}")
+        print(f"ZIP Code: {data['zip']}")
+        print(f"Latitude: {data['lat']}")
+        print(f"Longitude: {data['lon']}")
+        print(f"ISP: {data['isp']}\n")
         
         # Create a Google Maps link
         google_maps_url = f"https://www.google.com/maps?q={data['lat']},{data['lon']}"
-        print("Google Maps URL:", google_maps_url)
+        print(f"[Google Maps URL]\n{google_maps_url}")
     else:
         print("Failed to locate the IP address.")
 
 if __name__ == "__main__":
     ip_address = input("Enter an IP address: ")
     ip_locator(ip_address)
-    
+   
