6ed3a897dbb8c5eca00a9e616a0bf3d0d06b5c09
KiNGEX1 <144549465+KiNGEX1@users.noreply.github.com>
2023-10-01 00:36:44 +0530

    Update and rename Start.sh to start.sh

--- Start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if command -v python &>/dev/null; then
-    echo "Python is installed. Running Meta.py..."
-    python Meta.py
-else
-    echo "Python is required to run Meta.py. Please install Python."
-fi
