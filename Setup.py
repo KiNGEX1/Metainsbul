488ea072a3dad7b77e43bfd42593f830a4c23459
KiNGEX1 <144549465+KiNGEX1@users.noreply.github.com>
2023-10-29 21:24:30 +0530

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -1,7 +1,7 @@
 import os
 
 os.system('pkg install python -y')
-os.system('pip install request -y')
+os.system('pip install request')
 os.system('cd Metainsbul')
 os.system('mv MetaSh /data/data/com.termux/files/home')
 print("Type ./MetaSh to start")
