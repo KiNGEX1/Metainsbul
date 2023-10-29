310ea6648383873dc3da0aa7b56240b9549f24e9
KiNGEX1 <144549465+KiNGEX1@users.noreply.github.com>
2023-10-29 22:45:08 +0530

    Update and rename Setup.py to setup.sh

---
+++ setup.sh
@@ -0,0 +1,17 @@
+apt update && apt upgrade -y
+
+pkg install python -y
+
+pip install requests
+
+cd Metainsbul
+
+mv MetaSh /data/data/com.termux/files/home
+
+chmod +x /data/data/com.termux/files/home/MetaSh
+
+clear
+
+cd
+
+echo "Type ./MetaSh to start"
