7b86269ea8166c370eea8f944e33db39b7146606
KiNGEX1 <144549465+KiNGEX1@users.noreply.github.com>
2023-10-29 21:19:32 +0530

    Update and rename start.sh to Setup.py

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if command -v python &>/dev/null; then
-    echo "Python is installed. Running Metainsbul..."
-    python Meta.py
-else
-    echo "Python is required to run Metainsbul Please install Python."
-fi
